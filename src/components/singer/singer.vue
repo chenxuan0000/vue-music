05adca6fbab67587d4f573222fab36cfcad5a05b
chenxuan <chenxuan@kongzhongjr.com>
2017-08-20 15:55:59 +0800

    singer list jsonp接口数据拉取

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,6 +5,27 @@
 </template>
 
 <script>
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then(res => {
+          if(res.code === ERR_OK) {
+            console.log(res.data.list)
+          }
+        })
+      }
+    }
+  }
 
 </script>
 
