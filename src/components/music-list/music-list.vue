5f0433d4778be09e1602c3813b0a874f4250b1ba
chenxuan <chenxuan@kongzhongjr.com>
2017-08-31 18:25:38 +0800

    songlist 单ji处理显示播放器 增加vue action mapsAction工具类实现

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -17,7 +17,7 @@
     <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="ListenScroll" :data="songs" class="list"
             ref="list">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list @select="selectItem" :songs="songs"></song-list>
       </div>
       <div class="loading-container" v-show="!songs.length">
         <loading></loading>
@@ -31,6 +31,7 @@
   import SongList from 'base/song-list/song-list'
   import {prefixStyle} from 'common/js/dom'
   import loading from 'base/loading/loading'
+  import {mapActions} from 'vuex'
 
   const RESERVED_HEIGHT = 40
   const transform = prefixStyle('transform')
@@ -71,7 +72,16 @@
       },
       back() {
         this.$router.back()
-      }
+      },
+      selectItem(item, index) {
+        this.selectPlay({
+          list: this.songs,
+          index
+        })
+      },
+      ...mapActions([
+        'selectPlay'
+      ])
     },
     mounted() {
       this.imgHeight = this.$refs.bgImage.clientHeight
